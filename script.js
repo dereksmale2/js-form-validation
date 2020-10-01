ed4d320b4638769309cfc757e8abf715a61fe665
Derek Smale <dereksmale2@gmail.com>
2020-10-01 11:26:27 -0400

    final version

--- script.js
+++ script.js
@@ -4,27 +4,28 @@ let formIsValid;
 form.addEventListener("submit", validateForm);
 
 function validateForm(event) {
-  event.preventDefault();
-  let name = event.target[0].value;
-  let date = event.target[4].value;
-  let days = event.target[5].value;
-  let creditCard = event.target[6].value;
-  let cvv = event.target[7].value;
+    event.preventDefault();
+    let name = event.target[0].value;
+    let date = event.target[4].value;
+    let days = event.target[5].value;
+    let creditCard = event.target[6].value;
+    let cvv = event.target[7].value;
+    console.log(event)
 
-  formIsValid = true;
-  errorMessage("");
+    formIsValid = true;
+    errorMessage("");
 
-  validateCVV(cvv);
-  validateCardNumber(creditCard);
+    validateCVV(cvv);
+    validateCardNumber(creditCard);
 
-  if (formIsValid) {
+    if (formIsValid) {
     fetch("https://momentum-server.glitch.me/parking", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ formData: { name } }),
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ formData: { name } }),
     })
-      .then((res) => res.json())
-      .then((data) => {
+        .then((res) => res.json())
+        .then((data) => {
         let total = document.querySelector("#total");
         let amount = 0;
         days = parseInt(days);
@@ -36,61 +37,61 @@ function validateForm(event) {
         let loopDate = startDate;
 
         while (loopDate.format("M/D/YYYY") !== endDate.format("M/D/YYYY")) {
-          let weekday = moment(loopDate).weekday();
+            let weekday = moment(loopDate).weekday();
 
-          if (weekday === 6 || weekday === 0) {
+            if (weekday === 6 || weekday === 0) {
             amount += 7;
-          } else {
+            } else {
             amount += 5;
-          }
+            }
 
-          loopDate.add(1, "days");
+            loopDate.add(1, "days");
         }
 
         total.innerHTML = amount;
-      })
-      .catch((err) => {
+        })
+        .catch((err) => {
         console.log("error: ", err);
         errorMessage("Something went wrong. Please try again.");
-      });
-  }
+        });
+    }
 }
 
 function validateCardNumber(number) {
-  let regex = new RegExp("^[0-9]{16}$");
-  if (!regex.test(number)) {
+    let regex = new RegExp("^[0-9]{16}$");
+    if (!regex.test(number)) {
     formIsValid = false;
     errorMessage("Credit card was not valid");
-  }
-  return luhnCheck(number);
+    }
+    return luhnCheck(number);
 }
 
 function luhnCheck(val) {
-  let sum = 0;
-  for (let i = 0; i < val.length; i++) {
+    let sum = 0;
+    for (let i = 0; i < val.length; i++) {
     let intVal = parseInt(val.substr(i, 1));
     if (i % 2 == 0) {
       intVal *= 2;
-      if (intVal > 9) {
+        if (intVal > 9) {
         intVal = 1 + (intVal % 10);
-      }
+        }
     }
     sum += intVal;
-  }
-  return sum % 10 == 0;
+    }
+    return sum % 10 == 0;
 }
 
 function validateCVV(number) {
-  let regex = new RegExp("^[0-9]{3}$");
-  if (!regex.test(number)) {
+    let regex = new RegExp("^[0-9]{3}$");
+    if (!regex.test(number)) {
     formIsValid = false;
     errorMessage("CVV was not valid");
-  }
+    }
 }
 
 function errorMessage(message) {
-  let errorDiv = document.querySelector("#error-msg");
-  if (errorDiv) {
+    let errorDiv = document.querySelector("#error-msg");
+    if (errorDiv) {
     errorDiv.innerHTML = message;
-  }
+    }
 }
